998da7f25bf84ad1e9b76307d149b715c0a9b61d
MaorHadadLD <maorhadad94@gmail.com>
2025-05-04 18:34:27 +0300

    update

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-// import { registerApplication, start } from 'single-spa';
+
 ReactDOM.render(<App />, document.querySelector('#root'));
