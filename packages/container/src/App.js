0be084d6e22545ba8347961bebd1c212ea661cab
MaorHadadLD <maorhadad94@gmail.com>
2025-05-05 23:12:30 +0300

    Lazily Loading AubApps

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-import AuthApp from '../components/AuthApp';
-import MarketingApp from '../components/MarketingApp';
 import Header from '../components/Header';
+
+const MarketingLazy = lazy(() => import('../components/MarketingApp'));
+const AuthLazy = lazy(() => import('../components/AuthApp'));
 
 // Generate a unique class name for each component to avoid conflicts
 const generateClassName = createGenerateClassName({
@@ -17,10 +18,12 @@ export default () => {
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header />
+                    <Suspense fallback={<div>Loading...</div>}>
                     <Switch>
-                        <Route path="/auth" component={AuthApp} />
-                        <Route path="/" component={MarketingApp} />
+                        <Route path="/auth" component={AuthLazy} />
+                        <Route path="/" component={MarketingLazy} />
                     </Switch>
+                    </Suspense>
                 </div>
             </StylesProvider>
         </BrowserRouter>
