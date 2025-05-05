312eda2956a0e466ad32686460fd75540d3e39d3
MaorHadadLD <maorhadad94@gmail.com>
2025-05-05 22:09:33 +0300

    Added Signin & Signup

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {Switch, Route, Router} from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-
+import Signin from './components/SignIn';
+import Signup from './components/SignUp';
 
 // Generate a unique class name for each component to avoid conflicts
 const generateClassName = createGenerateClassName({
@@ -14,7 +15,8 @@ export default ({history}) => {
         <StylesProvider generateClassName={generateClassName}>
             <Router history={history}>
             <Switch>
-                
+                <Route path="/auth/signin" component={Signin} />
+                <Route path="/auth/signup" component={Signup} />
             </Switch>
             </Router>
         </StylesProvider>
