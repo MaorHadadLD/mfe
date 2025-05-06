1b36f69485ce70ea3ed6ae28ba69e77c11f1fb66
MaorHadadLD <maorhadad94@gmail.com>
2025-05-06 12:25:46 +0300

    fixing bug

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,32 +1,34 @@
-import React, {lazy, Suspense} from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
-import Progress from './components/Progress';
+import Progress from '../components/Progress';
 import Header from '../components/Header';
 
 const MarketingLazy = lazy(() => import('../components/MarketingApp'));
 const AuthLazy = lazy(() => import('../components/AuthApp'));
 
-// Generate a unique class name for each component to avoid conflicts
 const generateClassName = createGenerateClassName({
-    productionPrefix: 'co',
+  productionPrefix: 'co',
 });
 
 export default () => {
-    return (
-        <BrowserRouter>
-            <StylesProvider generateClassName={generateClassName}>
-                <div>
-                    <Header />
-                    <Suspense fallback={<Progress />}>
-                    <Switch>
-                        <Route path="/auth" component={AuthLazy} />
-                        <Route path="/" component={MarketingLazy} />
-                    </Switch>
-                    </Suspense>
-                </div>
-            </StylesProvider>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
 };
